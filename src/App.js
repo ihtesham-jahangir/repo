c76efb3d0a603d1887ff947b36841ad6f7c616ef
ihtesham jahangir <37655@students.riphah.edu.pk>
2023-09-04 21:40:26 +0500

    z

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>This is ihtesham jahangir</h1>
+        <h1>This is ihtesham jahangir   </h1>
         
       </header>
     </div>
