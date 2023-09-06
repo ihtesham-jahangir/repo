e4b722aca15970a7495c9582b61d235bd38e5b81
ihtesham jahangir <97870882+ihtesham-jahangir@users.noreply.github.com>
2023-09-06 23:05:37 +0500

    Adding Counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const[Count,SetCount] = useState(0);
+  let counter=()=>{
+    SetCount(Count+1);
+  }
+  let rest=()=>{
+    SetCount(0)
+  }
   return (
+ 
     <div className="App">
       <header className='App-header'> 
         <h1> Hi! This is Ihtesham Jahanghir </h1>
         <p> and I'm working on git. </p>
         <div className="container">
-        <button type="button" className="btn btn-primary btn-lg" > Say Hi! </button>
-        
+          <p>{Count}</p>
+        <button type="button" onClick={counter} className="btn btn-primary btn-lg" > Counter </button>
+        <button type="button" onClick={rest} className="btn btn-primary btn-lg" > Rest </button>
         </div>
       </header>
       
